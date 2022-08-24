d02eeb09201308c432417d2802d8113b0d4b58c6
runalb <65166862+runalb@users.noreply.github.com>
2022-08-24 16:27:33 +0530

    Dashboard setup

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from re import template
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
+
+def dashboard(request):
+    return render(request,'dashboard/index.html')
