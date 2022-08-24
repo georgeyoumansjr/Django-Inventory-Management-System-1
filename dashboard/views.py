5790350beed2cc73101d628c09e18986e0e33526
runalb <65166862+runalb@users.noreply.github.com>
2022-08-24 18:35:39 +0530

    Added dashboard links

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,3 @@
-from re import template
 from django.shortcuts import render, HttpResponse
 
 
@@ -7,3 +6,20 @@ from django.shortcuts import render, HttpResponse
 def dashboard_index(request):
     return render(request,'dashboard/index.html')
 
+def add_products(request):
+    return HttpResponse("add_products PAGE")
+
+def search_available_products(request):
+    return HttpResponse("search_available_products PAGE")
+
+def view_available_products(request):
+    return HttpResponse("view_available_products PAGE")
+
+def sell_available_products(request):
+    return HttpResponse("sell_available_products PAGE")
+
+def view_sold_products(request):
+    return HttpResponse("view_sold_products PAGE")
+
+def users(request):
+    return HttpResponse("users PAGE")
