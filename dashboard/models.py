462b4f33d4dab3e51e2a4bd7a872af42f8e6e623
runalb <65166862+runalb@users.noreply.github.com>
2022-08-31 17:59:27 +0530

    Added Available_product_table

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,9 @@
+from itertools import product
 from django.db import models
 
 # Create your models here.
+
+class Available_product_table(models.Model):
+    product_name = models.CharField(max_length=50)
+    product_price = models.CharField(max_length=50)
+    product_quantity = models.IntegerField()
