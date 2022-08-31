a0857077bf71ff291ca1d51f5e5de788a5fe7ba7
runalb <65166862+runalb@users.noreply.github.com>
2022-08-31 18:11:03 +0530

    string representation of model - Available_product_table

--- dashboard/models.py
+++ dashboard/models.py
@@ -7,3 +7,6 @@ class Available_product_table(models.Model):
     product_name = models.CharField(max_length=50)
     product_price = models.CharField(max_length=50)
     product_quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id} - {self.product_name} - {self.product_price} -{self.product_quantity}"
