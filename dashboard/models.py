2e53a5c8ce02354497f80a07b81a9729b923abfd
runalb <65166862+runalb@users.noreply.github.com>
2022-09-02 02:35:06 +0530

    Added Sold_product Model

--- dashboard/models.py
+++ dashboard/models.py
@@ -9,3 +9,14 @@ class Available_product_table(models.Model):
 
     def __str__(self):
         return f"{self.id} - {self.product_name} - {self.product_price} - {self.product_quantity}"
+
+
+class Sold_product_table(models.Model):
+    date_time = models.DateTimeField()
+    product_id = models.IntegerField()
+    product_name = models.CharField(max_length=50)
+    product_price = models.IntegerField()
+    product_quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id} - {self.date_time} - {self.product_id} - {self.product_name} - {self.product_price} - {self.product_quantity}"
