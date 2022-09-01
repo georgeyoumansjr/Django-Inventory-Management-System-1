41af32b1defe2d3bd04300da6e9f06dbd6d8eb4d
runalb <65166862+runalb@users.noreply.github.com>
2022-09-02 01:32:13 +0530

    Changed AddProductForm placeholder

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -2,9 +2,9 @@ from django import forms
 from .models import Available_product_table
 
 class AddProductForm(forms.ModelForm):
-    product_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Product Name'}))
-    product_price = forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control','placeholder':'Product Price'}))
-    product_quantity = forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control','placeholder':'Quantity'}))
+    product_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Product Name'}))
+    product_price = forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control','placeholder':'Enter Product Price'}))
+    product_quantity = forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control','placeholder':'Enter Quantity'}))
 
     class Meta():
         model = Available_product_table
